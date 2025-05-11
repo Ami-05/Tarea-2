1a767271b60da53525c28e10444ec71c83eca871
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-05-11 15:03:03 -0600

    Ejercicio 12.c

--- Ejercicio 12.c
+++ Ejercicio 12.c
@@ -7,24 +7,24 @@
 int main()
 {
     setlocale(LC_NUMERIC, "");
-int n, i, mul, producto=1;
-
-printf("Ingrese un número del cual se obtendra la multiplicación de todos los números antesesores a este: ");
-scanf("%d",&n);
-
-if(n<1)
-{
-    printf("El numero debe ser un valor positivo.\n");
-    return 1;
-}
-
-for (i=1;i<=n;i++)
-{
-    mul=producto*i;
-    producto=mul;
-}
-
-printf("La multiplicacion de los primeros %d numeros naturales es: %'11d\n", n, mul);
+    int n, i, mul, producto=1;
+    
+    printf("Ingrese un número del cual se obtendra la multiplicación de todos los números antesesores a este: ");
+    scanf("%d",&n);
+    
+    if(n<1)
+    {
+        printf("El numero debe ser un valor positivo.\n");
+        return 1;
+    }
+    
+    for (i=1;i<=n;i++)
+    {
+        mul=producto*i;
+        producto=mul;
+    }
+    
+    printf("La multiplicacion de los primeros %d numeros naturales es: %'11d\n", n, mul);
 
     return 0;
 }
