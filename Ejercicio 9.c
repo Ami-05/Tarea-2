4dc7ca227a21034511d6f95c2ec96f7067e20545
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-05-11 13:50:39 -0600

    Ejercicio 9.c

---
+++ Ejercicio 9.c
@@ -0,0 +1,19 @@
+//Cruz Apodaca Amiel Merari
+//El sguiente programa obtiene la tabla de multiplcar de un numero entero N, comenzando desde el 1 concluyendo en el 10
+
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    
+    printf("Ingrese el numero entero del cual se obtendra la tabal de multiplicar:");
+    scanf("%d", &N);
+    
+    for (int i=1; i <= 10; i++)
+    {
+        printf ("%d x %d = %d\n", N, i, N * i);
+    }
+
+    return 0;
+}
