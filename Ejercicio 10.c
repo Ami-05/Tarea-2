16820480179bad57c4610a6cf8f839b5a58c2943
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-05-11 15:06:25 -0600

    Ejercicio 10.c

--- Ejercicio 10.c
+++ Ejercicio 10.c
@@ -5,34 +5,37 @@
 
 int main()
 {
-int numero, positivos=0, negativos=0, nulos=0;
-printf("Ingrese 100 números, se contara cuantos son positivos, cuantos son negativos y cuantos son nulos.\n");
-
-for(int i=0; i<100; i++)
-{
-    printf("Numero %d: ", i + 1);
-    scanf("%d",&numero);
-
-if(numero>0)
-{
-positivos++;
-}
-else if (numero<0)
-{
-negativos++; 
-}
-else
-{
-nulos++;
-}
-}
-printf("Resultados;\n");
-printf("%d ",positivos);
-printf("números positivos.\n");
-printf("%d ",negativos);
-printf("números negativos.\n");
-printf("%d ",nulos);
-printf("números nulos.\n");
+    int numero, positivos=0, negativos=0, nulos=0;
+    
+    printf("Ingrese 100 números, se contara cuantos son positivos, cuantos son negativos y cuantos son nulos.\n");
+    
+    for(int i=0; i<100; i++)
+    {
+        printf("Numero %d: ", i + 1);
+        scanf("%d",&numero);
+        
+        if(numero>0)
+        {
+            positivos++;
+        }
+        
+        else if (numero<0)
+        {
+            negativos++; 
+        }
+        else
+        {
+            nulos++;
+        }
+    }
+    
+    printf("Resultados;\n");
+    printf("%d ",positivos);
+    printf("números positivos.\n");
+    printf("%d ",negativos);
+    printf("números negativos.\n");
+    printf("%d ",nulos);
+    printf("números nulos.\n");
 
 return 0;
 }
