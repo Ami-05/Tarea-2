cc5e5f31e2070214d07097e901e844a55b7c9a9b
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-05-11 14:47:22 -0600

    Ejercicio 12.c

---
+++ Ejercicio 12.c
@@ -0,0 +1,30 @@
+//Cruz Apodaca Amiel Merari
+//El siguiente programa calcula e imprime el producto de los N primeros numeros naturales
+
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_NUMERIC, "");
+int n, i, mul, producto=1;
+
+printf("Ingrese un número del cual se obtendra la multiplicación de todos los números antesesores a este: ");
+scanf("%d",&n);
+
+if(n<1)
+{
+    printf("El numero debe ser un valor positivo.\n");
+    return 1;
+}
+
+for (i=1;i<=n;i++)
+{
+    mul=producto*i;
+    producto=mul;
+}
+
+printf("La multiplicacion de los primeros %d numeros naturales es: %'11d\n", n, mul);
+
+    return 0;
+}
