58472e14e3b7efec4c9349afa10ab6822e8d4d65
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-05-11 14:24:33 -0600

    Ejercicio 11.c

---
+++ Ejercicio 11.c
@@ -0,0 +1,18 @@
+//Cruz Apodaca Amiel Merari
+//El siguiente programa calcula la suma de los numeros pares comprendidos entre el 10 y 50
+
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0;
+    
+for(int i = 10; i <= 50; i += 2)
+{
+    sum += i;
+}
+
+printf("La suma de los números pares comprendidos entre 10 y 50 es: %d\n", sum);
+
+    return 0;
+}
